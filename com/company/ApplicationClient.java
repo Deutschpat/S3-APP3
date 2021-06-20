a8e15f02be5c713489f00cdaca088e1ddf52dd7e
Deutschpat <norp2703@usherbrooke.ca>
2021-06-20 14:53:58 -0400

    good luck lol

--- com/company/ApplicationClient.java
+++ com/company/ApplicationClient.java
@@ -1,4 +1,40 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ApplicationClient {
+    /**
+     * Open and read a file, and return the lines in the file as a list
+     * of Strings.
+     * (Demonstrates Java FileReader, BufferedReader, and Java5.)
+     */
+    private String readFile(String filename)
+    {
+        String records = new String();
+        try
+        {
+            BufferedReader reader = new BufferedReader(new FileReader(filename));
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                records.concat(line);
+            }
+            reader.close();
+            return records;
+        }
+        catch (Exception e)
+        {
+            System.err.format("Exception occurred trying to read '%s'.", filename);
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String nomFile(File filename){
+        return filename.getName();
+    }
 }
--- com/company/TransportClient.java
+++ com/company/TransportClient.java
@@ -8,24 +8,56 @@ public class TransportClient {
     File f;
 
 
+    public void transport(String records, File fichier) {
+        byte[] bytes = records.getBytes();
+        byte[][] hey = divideArray(bytes,200);
+
+        //1.
+        fichier.getName();
+
+        //2.
+        EnTeteLisible(hey);
+    }
+
+    public static byte[][] divideArray(byte[] source, int chunksize) {
+
+
+        byte[][] ret = new byte[(int) Math.ceil(source.length / (double) chunksize)][chunksize];
+
+        int start = 0;
+        int parts = 0;
+
+        for (int i = 0; i < ret.length; i++) {
+            if (start + chunksize > source.length) {
+                System.arraycopy(source, start, ret[i], 0, source.length - start);
+            } else {
+                System.arraycopy(source, start, ret[i], 0, chunksize);
+            }
+            start += chunksize;
+            parts++;
+        }
+        return ret;
+    }
+
+
     //1.
-    public String nomFichier(File fichier, int numPaquet){
-        if(numPaquet==1){
+/*    public String nomFichier(File fichier){
+        if(fichier[0][0]){
             f = new File(fichier.getName());
             f.toString().trim();
             return String.valueOf(f);
         }
         return String.valueOf(f);
-    }
+    }*/
 
     //2.
-    public String EnTeteLisible(String paquet){
+    public String EnTeteLisible(byte[][] paquet){
         String enTete = cat("Are you not entertained?!?",paquet);
                 return enTete;
     }
 
-    public String cat(String a, String b) {
-        a += b;
+    public String cat(String a, byte[][] b) {
+        a += b.toString();
         return a;
     }
 
@@ -33,7 +65,7 @@ public class TransportClient {
         return String.valueOf(a) + "A" + b.toString(); //La lettre "A" sera utilisé pour séparer la numérotation du reste du paquet
     }
 
-    //3.
+/*    //3.
     public PriorityQueue<String> OrdonnerPaquet(File paquet, int nbPaquets){
         PriorityQueue<String> pQueue = new PriorityQueue<String>();
         int numerotation=0;
@@ -42,7 +74,7 @@ public class TransportClient {
             pQueue.add(cat2(numerotation, paquet));
         }
         return pQueue;
-    }
+    }*/
 
 
 }
