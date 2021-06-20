6597c911999a4817944887f6b0db1015772c3e74
Deutschpat <norp2703@usherbrooke.ca>
2021-06-20 16:50:30 -0400

    j

--- com/company/TransportServeur.java
+++ com/company/TransportServeur.java
@@ -1,4 +1,11 @@
 package com.company;
 
 public class TransportServeur {
+
+    public String accuseReception(){
+        String header = "paquet recu";
+        byte[] b = new byte[20];
+        String bs = b.toString();
+        return header += bs;
+    }
 }
